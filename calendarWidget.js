79b4e56721a0a1e2d1a4f279585b84ee058d8308
Volodymyr Sapsai <vsapsai@gmail.com>
2011-09-18 14:09:27 +0300

    Looks like this is preferred way to specify multiline constants in JavaScript

--- calendarWidget.js
+++ calendarWidget.js
@@ -71,17 +71,17 @@ function buildCalendarForMonth(month, destinationContainer)
 
 function buildGridPlaceholder(container)
 {
-	container.update("<table class=\"gridPlaceholder\">\
-						<thead><tr><th>M</th><th>T</th><th>W</th><th>T</th><th>F</th><th>S</th><th>S</th></tr></thead>\
-						<tbody id=\"calendarGrid\">\
-							<tr><td>29</td><td>30</td><td>31</td><td>1</td><td>2</td><td>3</td><td>4</td></tr>\
-							<tr><td>5</td><td>6</td><td>7</td><td>8</td><td>9</td><td>10</td><td>11</td></tr>\
-							<tr><td>12</td><td>13</td><td>14</td><td>15</td><td>16</td><td>17</td><td>18</td></tr>\
-							<tr><td>19</td><td>20</td><td>21</td><td>22</td><td>23</td><td>24</td><td>25</td></tr>\
-							<tr><td>26</td><td>27</td><td>28</td><td>29</td><td>30</td><td>1</td><td>2</td></tr>\
-							<tr><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td><td>9</td></tr>\
-						</tbody>\
-					</table>");
+	container.update("<table class=\"gridPlaceholder\">" +
+						"<thead><tr><th>M</th><th>T</th><th>W</th><th>T</th><th>F</th><th>S</th><th>S</th></tr></thead>" +
+						"<tbody id=\"calendarGrid\">" +
+							"<tr><td>29</td><td>30</td><td>31</td><td>1</td><td>2</td><td>3</td><td>4</td></tr>" +
+							"<tr><td>5</td><td>6</td><td>7</td><td>8</td><td>9</td><td>10</td><td>11</td></tr>" +
+							"<tr><td>12</td><td>13</td><td>14</td><td>15</td><td>16</td><td>17</td><td>18</td></tr>" +
+							"<tr><td>19</td><td>20</td><td>21</td><td>22</td><td>23</td><td>24</td><td>25</td></tr>" +
+							"<tr><td>26</td><td>27</td><td>28</td><td>29</td><td>30</td><td>1</td><td>2</td></tr>" +
+							"<tr><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td><td>9</td></tr>" +
+						"</tbody>" +
+					"</table>");
 }
 
 function buildCalendarGridForMonth(month, container, xOffset)
